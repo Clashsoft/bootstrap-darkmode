c07e22878294610cbead7817e5eec85d6cca59b6
Adrian Kunz <clashsoft@hotmail.com>
2020-02-13 18:27:01 +0100

    Encapsulate darkswitch in function

--- darkswitch.js
+++ darkswitch.js
@@ -1,15 +1,19 @@
-document.write(`
-<div class="custom-control custom-switch">
-<input type="checkbox" class="custom-control-input" id="darkSwitch">
-<label class="custom-control-label" for="darkSwitch">Dark Mode</label>
-</div>
-`);
+function writeDarkSwitch(config) {
+	document.write(`
+	<div class="custom-control custom-switch">
+	<input type="checkbox" class="custom-control-input" id="darkSwitch">
+	<label class="custom-control-label" for="darkSwitch">Dark Mode</label>
+	</div>
+	`);
 
-const darkSwitch = document.getElementById('darkSwitch');
+	const darkSwitch = document.getElementById('darkSwitch');
 
-darkSwitch.checked = getTheme() === 'dark';
-darkSwitch.onchange = () => {
-	setTheme(darkSwitch.checked ? 'dark' : 'light');
-};
+	darkSwitch.checked = themeConfig.getTheme() === 'dark';
+	darkSwitch.onchange = () => {
+		config.setTheme(darkSwitch.checked ? 'dark' : 'light');
+	};
 
-themeChangeHandlers.push(theme => darkSwitch.checked = theme === 'dark');
+	config.themeChangeHandlers.push(theme => darkSwitch.checked = theme === 'dark');
+
+	return darkSwitch;
+}
